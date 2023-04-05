51dee5af97993e0499ae12abb2b189912ee7e79b
chromodorididae <98478977+chromodorididae@users.noreply.github.com>
2023-04-04 22:13:44 -0600

    forms updated

--- client/src/pages/posts/Categories.js
+++ client/src/pages/posts/Categories.js
@@ -40,16 +40,6 @@ export default function Categories() {
             </ul> */}
 
 <form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
-    </div>
-  </div>
   <div class="form-group">
     <label for="inputAddress">Address</label>
     <input type="text" class="form-control" id="inputAddress" placeholder="1234 Example St"/>
@@ -75,6 +65,7 @@ export default function Categories() {
       <input type="text" class="form-control" id="inputZip"/>
     </div>
   </div>
+  <br />
   <div class="form-group">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" id="gridCheck"/>
