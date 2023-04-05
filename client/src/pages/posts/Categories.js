e766facfeb3c196c490c450553b2097444f6efc9
chromodorididae <98478977+chromodorididae@users.noreply.github.com>
2023-04-04 22:01:51 -0600

    bootstrap added

--- client/src/pages/posts/Categories.js
+++ client/src/pages/posts/Categories.js
@@ -28,6 +28,7 @@ export default function Categories() {
         <div>
             <>
             <h1 style={{textAlign: 'center'}}>Tasty Co. Careers</h1>
+            <h6 style={{textAlign: 'center', fontStyle: 'italic'}}>bootstrap form</h6>
             </>
 
             {/* button, x.category value - search terms, onclick */}
@@ -38,6 +39,51 @@ export default function Categories() {
                 </li>)}
             </ul> */}
 
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Password</label>
+      <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Example St"/>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" id="inputCity"/>
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">State</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip"/>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck"/>
+      <label class="form-check-label" for="gridCheck">
+        Sign up for news and announcements
+      </label>
+    </div>
+  </div>
+</form>
             
             <form>
             <div>
